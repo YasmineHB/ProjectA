3bcb7c0c1178270baacce81c83a537c66557afbd
aaldu <aaldu@DESKTOP-26OB3J1.myfiosgateway.com>
2024-01-03 21:30:28 -0500

    Implemented add()

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -1,12 +1,17 @@
 package a;
 
 public class DemoA {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Monday");
-		
-		System.out.println("Sprint Work 1...");
-		System.out.println("Sprint Work 2...");
+
+		System.out.println("Sprint 1 Work ...");
+		System.out.println("Sprint 2 Work ...");
+	}
+
+	// Sprint 2 automation
+	public static void add(int a, int b) {
+		System.out.println(a + b);
 	}
 
 }
