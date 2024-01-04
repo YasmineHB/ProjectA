abfc0dc578196139c6b426bc642860ea93e47c6b
aaldu <aaldu@DESKTOP-26OB3J1.myfiosgateway.com>
2024-01-03 20:36:12 -0500

    Any Commit Message

---
+++ src/a/DemoA.java
@@ -0,0 +1,12 @@
+package a;
+
+public class DemoA {
+	
+	public static void main(String[] args) {
+		System.out.println("Monday");
+		
+		System.out.println("Sprint Work 1...");
+		System.out.println("Sprint Work 2...");
+	}
+
+}
